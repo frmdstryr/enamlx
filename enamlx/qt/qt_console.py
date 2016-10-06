ad6503866b41e92a08952edad101ec3b96338274
frmdstryr <frmdstryr@gmail.com>
2016-10-06 19:53:36 -0400

    Improve console

--- enamlx/qt/qt_console.py
+++ enamlx/qt/qt_console.py
@@ -10,17 +10,17 @@ from atom.api import Instance, ForwardTyped
 
 from ..widgets.console import ProxyConsole
 
-from enaml.qt import QtCore
+from enaml.qt import QtCore, QT_API
 from enaml.qt.qt_control import QtControl
 
 def console_factory():
     try:
-        from qtconsole.rich_jupyter_widget import RichJupyterWidget as ConsoleWidget
+        from qtconsole.rich_jupyter_widget import RichJupyterWidget as RichIPythonWidget
     except ImportError:
-        from IPython.qt.console.rich_ipython_widget import RichIPythonWidget as ConsoleWidget
-    
+        from IPython.qt.console.rich_ipython_widget import RichIPythonWidget
+        
     # TODO: Support IPython < 3
-    return ConsoleWidget
+    return RichIPythonWidget
 
 def kernel_factory():
     try:
@@ -34,17 +34,35 @@ class QtConsole(QtControl,ProxyConsole):
     #: Viewer widget
     widget = ForwardTyped(console_factory)
     
+    #: Kernel
+    kernel = Instance(object)
+    
     _sigint_timer = Instance(QtCore.QTimer)
     
     def create_widget(self):
+        d = self.declaration
         WidgetClass = console_factory()
-        self.widget = WidgetClass(self.parent_widget())
+        self.widget = WidgetClass(
+            parent = self.parent_widget(),
+            gui_completion = d.completion,
+            display_banner = d.display_banner
+        )
+        
         
     def init_widget(self):
         d = self.declaration
-        
         self.init_kernel()
         self.init_signal()
+        self.set_console_size(d.console_size)
+        
+        if d.font_family:
+            self.set_font_family(d.font_family)
+        if d.font_size:
+            self.set_font_size(d.font_size)
+        if d.buffer_size:
+            self.set_buffer_size(d.buffer_size)
+        if d.scope:
+            self.set_scope(d.scope)
         
     def init_kernel(self):
         d = self.declaration
@@ -52,14 +70,13 @@ class QtConsole(QtControl,ProxyConsole):
         kernel_manager = KernelManager()
         kernel_manager.start_kernel(show_banner=False)
         kernel = kernel_manager.kernel
-        kernel.gui = d.gui
-        
+        kernel.gui = 'qt4'
+        self.kernel = kernel
         kernel_client = kernel_manager.client()
         kernel_client.start_channels()
 
         self.widget.kernel_manager = kernel_manager
         self.widget.kernel_client = kernel_client
-        
         
     def init_signal(self):
         """allow clean shutdown on sigint"""
@@ -73,8 +90,30 @@ class QtConsole(QtControl,ProxyConsole):
         timer.start(200)
         # hold onto ref, so the timer doesn't get cleaned up
         self._sigint_timer = timer
-
-
+    
+    def set_font_size(self, size):
+        self.widget.font_size = size
         
+    def set_font_family(self, family):
+        self.widget.font_family = family
+    
+    def set_console_size(self, size):
+        self.widget.width = size.width
+        self.widget.height = size.height
         
+    def set_buffer_size(self, size):
+        self.widget.buffer_size = size
+        
+    def set_scope(self, state):
+        self.kernel.shell.push(state)
+        
+    def set_display_banner(self, enabled):
+        self.widget.display_banner = enabled
+        
+    def set_completion(self, mode):
+        self.widget.gui_completion = mode
+        
+    def set_execute(self, *args,**kwargs):
+        self.kernel.shell.execute(*args,**kwargs)
+
         
