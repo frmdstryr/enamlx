ce7de2ab8356e846f162fb1a5bb783870368e3f6
frm dstryr <frmdstryr@protonmail.com>
2023-07-26 09:36:48 -0400

    Drop func/def rename middleware

--- enamlx/__init__.py
+++ enamlx/__init__.py
@@ -1,25 +1,6 @@
-def install(allow_def=False):
+def install():
     """Install the toolkit factory widgets for the widgets provided
     by this library.
 
     """
     from enamlx.qt import qt_factories  # noqa: F401
-
-    if allow_def:
-        allow_def_funcs()
-
-
-def allow_def_funcs():
-    """Installs a patch to the parser so python's def keyword can be
-    used instead of enaml's func keyword.
-
-    Notes
-    ------
-    Use this at your own risk!  This was a feature intentionally
-    dismissed by the author of enaml because declarative func's are not the
-    same as python functions.
-
-    """
-    from enamlx.core import middleware
-
-    middleware.add_token_stream_processor(middleware.convert_enamldef_def_to_func)
